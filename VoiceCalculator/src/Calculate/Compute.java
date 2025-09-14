7cf3210ab585d4d23cf5d9e40bd9f1aeea188f15
Steve-wyk <2592709081@qq.com>
2025-09-14 23:11:44 +0800

    第一次修改

--- VoiceCalculator/src/Calculate/Compute.java
+++ VoiceCalculator/src/Calculate/Compute.java
@@ -8,6 +8,7 @@ public class Compute {
 		Expression express=new ExpressionBuilder(expression).build();
 		double result=express.evaluate();
 		String answer=String.format("%.2f", result);
+                                System.out.println("zh 是人机");
 		return answer;
 	}
 }
